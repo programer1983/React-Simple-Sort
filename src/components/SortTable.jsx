21823ae2eb40614396fb06d937241a75e012d49a
programer1983 <programer972@gmail.com>
2022-04-05 12:43:16 +0300

    commit-4;

--- src/components/SortTable.jsx
+++ src/components/SortTable.jsx
@@ -1,8 +1,30 @@
-import data from "./../MOCK_DATA.json"
+import {useState} from "react"
+import MokData from "./../MOCK_DATA.json"
 
 export default function SortTable() {
+  const [data, setData] = useState(MokData)
+
   return (
-    <div>SortTable</div>
+    <div>
+      <table>
+        <thead>
+          <th>First Name</th>
+          <th>Last Name</th>
+          <th>Email</th>
+          <th>Gender</th>
+        </thead>
+        <tbody>
+          {data.map((d) => (
+            <tr key={d.id}>
+              <td>{d.first_name}</td>
+              <td>{d.last_name}</td>
+              <td>{d.email}</td>
+              <td>{d.gender}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
   )
 }
 
