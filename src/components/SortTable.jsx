b0a52031502baea437afa48418054785d4dd7dcf
programer1983 <programer972@gmail.com>
2022-04-05 13:35:36 +0300

    commit-6;

--- src/components/SortTable.jsx
+++ src/components/SortTable.jsx
@@ -5,14 +5,32 @@ export default function SortTable() {
   const [data, setData] = useState(MokData)
   const [order, setOrder] = useState("ASC")
 
+  const sorting = (col) => {
+    if(order === "ASC"){
+      const sorted = [...data].sort((a, b) =>
+         a[col].toLowerCase() > b[col].toLowerCase() ? 1 : -1
+      )
+      setData(sorted)
+      setOrder("DSC")
+    }
+
+    if(order === "DSC"){
+      const sorted = [...data].sort((a, b) =>
+         a[col].toLowerCase() < b[col].toLowerCase() ? 1 : -1
+      )
+      setData(sorted)
+      setOrder("ASC")
+    }
+  }
+
   return (
     <div className="container">
       <table className="table table-bordered">
         <thead>
-          <th>First Name</th>
-          <th>Last Name</th>
-          <th>Email</th>
-          <th>Gender</th>
+          <th onClick={() => sorting("first_name")} className="click">First Name</th>
+          <th onClick={() => sorting("last_name")} className="click">Last Name</th>
+          <th onClick={() => sorting("email")} className="click">Email</th>
+          <th onClick={() => sorting("gender")} className="click">Gender</th>
         </thead>
         <tbody>
           {data.map((d) => (
